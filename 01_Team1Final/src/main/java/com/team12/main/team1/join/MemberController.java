ba2c37951a818024623fadcbb4dd757c02f248ec
Lim DongHoon <dla_ehdgns@naver.com>
2022-08-02 17:22:24 +0900

    풀 하기 위한 커밋

---
+++ 01_Team1Final/src/main/java/com/team12/main/team1/join/MemberController.java
@@ -0,0 +1,31 @@
+package com.team12.main.team1.join;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dh.pj.member.Member;
+
+@Controller
+public class MemberController {
+	
+	private MemberjoinDAO mlDAO;
+	
+	@RequestMapping(value = "/member.join", method = RequestMethod.POST)
+	public String memberJoin(Member m, HttpServletRequest req) {
+		
+//		mlDAO.join(m, req);
+//		mlDAO.loginCheck(req);
+//		req.setAttribute("contentPage", "member/join.jsp");
+//		
+//		return "index";
+//	}
+	
+
+
+	
+
+	
+}
---
+++ 01_Team1Final/src/main/java/com/team12/main/team1/join/Team1joinMapper.java
@@ -0,0 +1,5 @@
+package com.team12.main.team1.join;
+
+public interface Team1joinMapper {
+
+}
