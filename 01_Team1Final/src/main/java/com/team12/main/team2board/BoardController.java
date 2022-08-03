e7963b93ef7c3254af7b395be369e086037594ce
Lita Jeon <lita.y.jeon@gmail.com>
2022-08-03 16:37:43 +0900

    미안해요~~~~

--- 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
+++ 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
@@ -8,25 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BoardController {
-<<<<<<< HEAD
-    
-	
 
 	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
-		System.out.println(111);
-		System.out.println(111);
-		System.out.println(111);
-		System.out.println(2222);
-		System.out.println(222);
-		System.out.println(222);
-		System.out.println(222);
-=======
-
-	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
-	public String home(HttpServletRequest req) {
-		
->>>>>>> 2485cdb (create board DB)
 		
 	  req.setAttribute("contentPage", "2Team/board_jsp/board_list.jsp");
 	  return "2Team/t2_index";
