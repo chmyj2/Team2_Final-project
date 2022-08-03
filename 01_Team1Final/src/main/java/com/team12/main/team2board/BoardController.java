836f14f03fd7d70d094f59fb12bd40f6cdbbd0b8
WonbongKIM <jinyuanfeng5@gmail.com>
2022-08-03 15:37:28 +0900

    no message

--- 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
+++ 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
@@ -2,7 +2,6 @@ package com.team12.main.team2board;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BoardController {
     
-	@Autowired
-	private Team2BoardDAO bDAO;
 	
 
 	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
