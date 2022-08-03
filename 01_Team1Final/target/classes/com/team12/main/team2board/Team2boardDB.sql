d506f65aa67bb91970af205ad5ffa649c2746baf
WonbongKIM <jinyuanfeng5@gmail.com>
2022-08-03 13:07:27 +0900

    create boardDTO, boardDAO

--- 01_Team1Final/target/classes/com/team12/main/team2board/Team2boardDB.sql
+++ 01_Team1Final/target/classes/com/team12/main/team2board/Team2boardDB.sql
@@ -13,3 +13,19 @@ create sequence team2_board_seq;
 insert into team2_board values(team2_board_seq.nextval, 'mz', '1', 'title', 'txt', sysdate, 'karina.jpg')
 
 select * from team2_board
+
+select * 
+from (
+    select rownum as rn, board_num, board_member_id, board_category, board_title, board_txt, board_date, board_img
+     from(
+     select * from team2_board
+     where board_category = '1'
+     and board_title like '%%'
+     order by board_date desc 
+   )
+) where rn between 1 and 20
+
+select count(*)
+from team2_board
+where board_category = '1'
+and board_title like '%%'
