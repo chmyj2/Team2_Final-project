41431467d2d97d17c1858cd7ac540c89f13874e7
WonbongKIM <jinyuanfeng5@gmail.com>
2022-08-03 16:01:27 +0900

    commit

--- 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
+++ 01_Team1Final/src/main/java/com/team12/main/team2board/BoardController.java
@@ -8,32 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BoardController {
-<<<<<<< HEAD
     
 	
 
-	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
-	public String home(HttpServletRequest req) {
-<<
-		System.out.println(111);
-		System.out.println(111);
-		System.out.println(111);
-		System.out.println(2222);
-		System.out.println(222);
-		System.out.println(222);
-		System.out.println(222);
-<<<<<<< HEAD
-==
-		
->>
-=======
-=======
 
 	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
 		
->>>>>>> 2485cdb (create board DB)
->>>>>>> 7efdbd5827e375331b74f31dfe9f49f909b7165e
 		
 	  req.setAttribute("contentPage", "2Team/board_jsp/board_list.jsp");
 	  return "2Team/t2_index";
