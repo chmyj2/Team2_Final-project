29c05549c37f1706963f2c7d42c6da728a815262
yoky <leeyoky@naver.com>
2022-08-03 13:06:12 +0900

    your the best.....

--- 01_Team1Final/src/main/java/com/team12/main/t1/Team1HomeController.java
+++ 01_Team1Final/src/main/java/com/team12/main/t1/Team1HomeController.java
@@ -10,26 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Team1HomeController {
 	
-<<<<<<< HEAD
+
 	@RequestMapping(value = "/team1Main", method = {RequestMethod.GET, RequestMethod.POST})
-=======
-	@RequestMapping(value = "/team1Main", method = RequestMethod.GET)
->>>>>>> cdae99558cacacd9933946fce6b5454938511cc5
 	public String home(HttpServletRequest req) {
 		
-		
 		req.setAttribute("contentPage", "t1_home.jsp");
-		
 		return "1Team/t1_index";
 	}
 	
-<<<<<<< HEAD
-=======
-	@RequestMapping(value = "/index.go", method = RequestMethod.GET)
-	public String indexGo(HttpServletRequest req) {
-		return home(req);
-	}
->>>>>>> cdae99558cacacd9933946fce6b5454938511cc5
+
 	
 }
 
