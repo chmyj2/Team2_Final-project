2d7e9a7199e68d9aacb6df7d3248c829baf0c94c
Lim DongHoon <dla_ehdgns@naver.com>
2022-08-03 10:36:30 +0900

    풀 하기 위한 커밋

--- 01_Team1Final/src/main/java/com/team12/main/team1/join/Member.java
+++ 01_Team1Final/src/main/java/com/team12/main/team1/join/Member.java
@@ -1,111 +1,66 @@
 package com.team12.main.team1.join;
 
-import java.util.Date;
 
 public class Member {
-
-	private String member_detail_num;
-	private String member_name;
-	private String member_birth;
-	private String member_sex;
-	private String member_phoneNum;
-	private String member_Address;
-	private String member_eMail;
-	private Date member_joinDate;
-	private String member_paper;
+	
+	private String m_id;
+	private String m_pw;
+	private String m_name;
+	private String m_addr;
+	private String m_photo;
 	
 	public Member() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Member(String member_detail_num, String member_name, String member_birth, String member_sex,
-			String member_phoneNum, String member_Address, String member_eMail, Date member_joinDate,
-			String member_paper) {
+	public Member(String m_id, String m_pw, String m_name, String m_addr, String m_photo) {
 		super();
-		this.member_detail_num = member_detail_num;
-		this.member_name = member_name;
-		this.member_birth = member_birth;
-		this.member_sex = member_sex;
-		this.member_phoneNum = member_phoneNum;
-		this.member_Address = member_Address;
-		this.member_eMail = member_eMail;
-		this.member_joinDate = member_joinDate;
-		this.member_paper = member_paper;
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_name = m_name;
+		this.m_addr = m_addr;
+		this.m_photo = m_photo;
 	}
 
-	public String getMember_detail_num() {
-		return member_detail_num;
+	public String getM_id() {
+		return m_id;
 	}
 
-	public void setMember_detail_num(String member_detail_num) {
-		this.member_detail_num = member_detail_num;
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
 	}
 
-	public String getMember_name() {
-		return member_name;
+	public String getM_pw() {
+		return m_pw;
 	}
 
-	public void setMember_name(String member_name) {
-		this.member_name = member_name;
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
 	}
 
-	public String getMember_birth() {
-		return member_birth;
+	public String getM_name() {
+		return m_name;
 	}
 
-	public void setMember_birth(String member_birth) {
-		this.member_birth = member_birth;
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
 	}
 
-	public String getMember_sex() {
-		return member_sex;
+	public String getM_addr() {
+		return m_addr;
 	}
 
-	public void setMember_sex(String member_sex) {
-		this.member_sex = member_sex;
+	public void setM_addr(String m_addr) {
+		this.m_addr = m_addr;
 	}
 
-	public String getMember_phoneNum() {
-		return member_phoneNum;
+	public String getM_photo() {
+		return m_photo;
 	}
 
-	public void setMember_phoneNum(String member_phoneNum) {
-		this.member_phoneNum = member_phoneNum;
+	public void setM_photo(String m_photo) {
+		this.m_photo = m_photo;
 	}
-
-	public String getMember_Address() {
-		return member_Address;
-	}
-
-	public void setMember_Address(String member_Address) {
-		this.member_Address = member_Address;
-	}
-
-	public String getMember_eMail() {
-		return member_eMail;
-	}
-
-	public void setMember_eMail(String member_eMail) {
-		this.member_eMail = member_eMail;
-	}
-
-	public Date getMember_joinDate() {
-		return member_joinDate;
-	}
-
-	public void setMember_joinDate(Date member_joinDate) {
-		this.member_joinDate = member_joinDate;
-	}
-
-	public String getMember_paper() {
-		return member_paper;
-	}
-
-	public void setMember_paper(String member_paper) {
-		this.member_paper = member_paper;
-	}
-	
-	
 	
 	
 	
