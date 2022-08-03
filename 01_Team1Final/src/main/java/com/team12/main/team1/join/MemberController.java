941365e9124adaa1b2620cc647cee4d51d26b666
LitaHart <lita.y.jeon@gmail.com>
2022-08-03 10:02:19 +0900

    Merge branch 'Dev' of https://github.com/LitaHart/Team1FinalProject into YUJIN
    # Conflicts:
    #	01_Team1Final/target/m2e-wtp/web-resources/META-INF/maven/com.team1/main/pom.properties

---
+++ 01_Team1Final/src/main/java/com/team12/main/team1/join/MemberController.java
@@ -0,0 +1,30 @@
+package com.team12.main.team1.join;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MemberController {
+	
+	private MemberjoinDAO mlDAO;
+	
+	@RequestMapping(value = "/member.join", method = RequestMethod.POST)
+	public String memberJoin(Member m, HttpServletRequest req) {
+		
+		mlDAO.join(m, req);
+//		mlDAO.loginCheck(req);
+		req.setAttribute("contentPage", "LDH/DH_login.jsp");
+		
+		return "LDH/DH_login";
+	}
+	
+
+
+	
+
+	
+}
