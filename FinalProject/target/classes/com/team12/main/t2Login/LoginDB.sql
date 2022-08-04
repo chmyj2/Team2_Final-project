f4c368e00dd3aa9bedff54363d758b1c09edab7d
sujin <gks365199@gmail.com>
2022-08-04 16:56:15 +0900

    Revert "[modify] member DB"
    This reverts commit d32de30e0c966d814e77c08b531e49ea9762355c.

--- FinalProject/target/classes/com/team12/main/t2Login/LoginDB.sql
+++ FinalProject/target/classes/com/team12/main/t2Login/LoginDB.sql
@@ -1,5 +1,6 @@
 create table final_member(
-member_ID varchar2(30 char) primary key,
+member_num number(6) primary key,
+member_ID varchar2(30 char) unique,
 member_PW varchar2(30 char) not null,
 member_linkWhere number(3) not null,
 member_name varchar2(30 char) not null,
@@ -18,34 +19,9 @@ select * from final_member;
 
 create sequence final_member_seq;
 
-insert into final_member values('mz','1004',1,'mz','2022-08-04',1,01012341234,'종로','mz@naver.com',sysdate,1);
+insert into final_member values(final_member_seq.nextval,'mz','1004',1,'mz','2022-08-04',1,01012341234,'종로','mz@naver.com',sysdate,1);
 
 create table pet_information(
 baby_num number(6) primary key,
-baby_memberNum varchar2(30 char) not null,
-baby_name varchar2(30 char) not null,
-baby_birth date not null,
-baby_sex varchar2(30 char) not null,
-baby_weight number(4,1)not null,
-baby_type varchar2(30 char)not null,
-baby_typeDetail varchar2(30 char)not null,
-baby_neut varchar2(30 char)not null,
-baby_img varchar2(100 char),
 
-constraint baby_ownerNum_fk foreign key(baby_memberNum) 
-    references final_member(member_ID) 
-    on delete cascade
 )
-
-drop table pet_information;
-
-create sequence pet_information_seq;
-
-select * from PET_INFORMATION;
-
-insert into pet_information values(pet_information_seq.nextval,'mz','동이','2021-05-01','암컷','20.1','강아지','진돗개','무','a.jpg');
-
-
-
-
-
