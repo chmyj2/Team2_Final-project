2c621516d0e4ea111ccb36ae3763388b6f8d95f6
WonbongKIM <jinyuanfeng5@gmail.com>
2022-08-04 10:31:43 +0900

    update board_jsp

--- FinalProject/src/main/java/com/team12/main/team2board/Team2BoardDAO.java
+++ FinalProject/src/main/java/com/team12/main/team2board/Team2BoardDAO.java
@@ -1,5 +1,9 @@
 package com.team12.main.team2board;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,16 +14,36 @@ public class Team2BoardDAO {
 	@Autowired
 	private SqlSession ss;
 	
-	private int allPost;
 	
 	
-	public int countPost() {
+	public int countPost(HttpServletRequest req, Team2BoardDTO board) {
 		
-		allPost = ss.getMapper(Team2BoardMapper.class).getAllpostCount();
+	int	allPost = ss.getMapper(Team2BoardMapper.class).getAllpostCount(board);
 		
-		return allPost;
+		int totalPage = (int) Math.ceil((double)allPost/10);
+		
+		req.setAttribute("r", totalPage);
+		
+		return totalPage;
 	}
 
+
+
+	public void showPostList(HttpServletRequest req, Team2BoardDTO board) {
+		
+		List<Team2BoardDTO> posts = ss.getMapper(Team2BoardMapper.class).getPostList(board);
+		
+		req.setAttribute("posts", posts);
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
