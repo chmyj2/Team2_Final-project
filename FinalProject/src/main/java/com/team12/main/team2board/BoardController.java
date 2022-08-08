85bd27bdcd1ece3d4f751782990504fe9d76d7cc
Lita Jeon <lita.y.jeon@gmail.com>
2022-08-08 15:42:42 +0900

    2 dh (#137)
    * 서블릿 수정
    
    * 풀 하기 위한 커밋
    
    * 풀 하기 위한 커밋
    
    Co-authored-by: Lim DongHoon <dla_ehdgns@naver.com>

--- FinalProject/src/main/java/com/team12/main/team2board/BoardController.java
+++ FinalProject/src/main/java/com/team12/main/team2board/BoardController.java
@@ -1,14 +1,6 @@
 package com.team12.main.team2board;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.gson.JsonObject;
-
 @Controller
 public class BoardController {
 
 	@Autowired
 	private Team2BoardDAO bDAO;
 
-	@RequestMapping(value = "team2.boardlist", method = RequestMethod.GET)
+	@RequestMapping(value = "/team2.boardlist", method = RequestMethod.GET)
 	public String boardList(HttpServletRequest req, Team2BoardDTO board) {
 
 		bDAO.countPost(req, board);
@@ -35,7 +25,7 @@ public class BoardController {
 		return "2Team/t2_index";
 	}
 
-	@RequestMapping(value = "team2.createPostPage", method = RequestMethod.GET)
+	@RequestMapping(value = "/team2.createPostPage", method = RequestMethod.GET)
 	public String createPostPage(HttpServletRequest req, Team2BoardDTO board) {
 
 		req.setAttribute("contentPage", "board_jsp/board_create.jsp");
@@ -43,7 +33,7 @@ public class BoardController {
 		return "2Team/t2_index";
 	}
 
-	@RequestMapping(value = "team2.createPost", method = RequestMethod.POST)
+	@RequestMapping(value = "/team2.createPost", method = RequestMethod.POST)
 	public String createPost(HttpServletRequest req, Team2BoardDTO board) {
 
 		bDAO.countPost(req, board);
