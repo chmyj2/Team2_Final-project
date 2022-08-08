80479fdcc0bfa4ad0fdf89ec2abfef96c8f5eb85
WonbongKIM <jinyuanfeng5@gmail.com>
2022-08-08 16:33:15 +0900

    DB 변경

--- FinalProject/src/main/java/com/team12/main/team2board/BoardController.java
+++ FinalProject/src/main/java/com/team12/main/team2board/BoardController.java
@@ -1,14 +1,7 @@
 package com.team12.main.team2board;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.google.gson.JsonObject;
 
 @Controller
 public class BoardController {
