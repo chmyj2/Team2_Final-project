5cba1411762b331e3cc11ee921514793589c1595
sujin <gks365199@gmail.com>
2022-08-12 18:41:31 +0900

    [create naver login] naver login Good

--- FinalProject/src/main/java/com/team12/main/t2Login/LoginController.java
+++ FinalProject/src/main/java/com/team12/main/t2Login/LoginController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.team12.main.t2.Team2HomeController;
+
+
 @Controller
 public class LoginController {
 	
@@ -185,11 +188,36 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value = "/naverlogin.check", method = RequestMethod.GET)
+	@ResponseBody
 	public int naverlogincheck(HttpServletRequest req, Membert2 m) {
-		//네이버로그인 확인
-		
+		//네이버 아이디 확인
 		return lDAO.naverLogin(m);
 	}
+	
+	@RequestMapping(value = "/naverlogin.do", method = RequestMethod.GET)
+	public String naverloginDo(HttpServletRequest req, Membert2 m) {
+			//네이버 로그인하기
+			if (req.getParameter("member_ID") != null) {
+				lDAO.loginNaver(req, m);
+				lDAO.loginCheck(req);
+			}
+			req.setAttribute("contentPage", "t2_home.jsp");	
+			
+		return "2Team/t2_index";
+	}
+	
+	@RequestMapping(value = "/naverjoin.go", method = RequestMethod.GET)
+	public String naverjoingo(HttpServletRequest req, Membert2 m) {
+		//네이버 회원가입하러가기
+		
+		lDAO.loginCheck(req);
+		req.setAttribute("contentPage", "t2login/t2_join.jsp");
+		
+		
+		return "2Team/t2_index";
+	}
+	
+	
 	
 	
 	
--- FinalProject/src/main/webapp/WEB-INF/views/2Team/t2login/naverLogin.jsp
+++ FinalProject/src/main/webapp/WEB-INF/views/2Team/t2login/naverLogin.jsp
@@ -20,21 +20,25 @@
     alert(naver_id_login.getProfileData('email'));
     let email =naver_id_login.getProfileData('email');
     let member_ID =  email.split('@');
-    alert(member_ID[0]);
+   	member_ID =member_ID[0];
+    alert(member_ID);
     
     $.ajax({
 		url:"naverlogin.check",
 		type:"GET",
 		dataType :"text",
-		data:{"member_ID":member_ID[0]},
+		data:{"member_ID":member_ID},
 		success: function(getData) {
 			console.log(getData);
 			if (getData >=1) {
 				//회원정보가 있다는 의미
 				window.close();
-				window.open 아이디? 홈으로 돌리기전 
+				window.opener.location.href="http://localhost/main/naverlogin.do?member_ID="+member_ID;
 			}else {
 				//회원정보가 없다는 의미
+				alert('회원가입을 해주세요');
+				window.close();
+				window.opener.location.href="http://localhost/main/naverjoin.go?member_ID="+member_ID;
 			}
 			
 		},
@@ -43,8 +47,6 @@
 			
 		}
 	}); // ajax끝
-    
-    
   }
 </script>
 </body>
