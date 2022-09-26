cb1aa3745fc75c2de98d527951590f06517e0e11
최용준 <>
2022-09-26 23:16:47 +0900

    last commit

--- FinalProject/src/main/webapp/resources/t2_js/hospital_function2.js
+++ FinalProject/src/main/webapp/resources/t2_js/hospital_function2.js
@@ -3,7 +3,7 @@ $(function(){
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = {
 		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-		level: 4 // 지도의 확대 레벨
+		level: 8 // 지도의 확대 레벨
 };  
 
 // 지도를 생성합니다    
